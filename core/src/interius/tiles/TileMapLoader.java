0fc9975bdf8dbb090f5f6af4959798b485ead33d
Roko Milić <cybermen21@hotmail.com>
2017-02-18 21:05:14 +0100

    Map collisions

--- core/src/interius/tiles/TileMapLoader.java
+++ core/src/interius/tiles/TileMapLoader.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
@@ -18,7 +19,9 @@ public class TileMapLoader {
     private OrthographicCamera camera;
     
     private TiledMap map;
+    private TiledMapTileLayer collisionLayer;
     private TiledMapRenderer tiledMapRenderer;
+    private boolean[][] collision;
     
     public TileMapLoader(String path) {
         map = new TmxMapLoader().load(path);
@@ -29,6 +32,17 @@ public class TileMapLoader {
         
         tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
         tiledMapRenderer.setView(camera);
+        collisionLayer = (TiledMapTileLayer) map.getLayers().get("collision");
+        collision = new boolean[getHeight()][getWidth()];
+        for (int i = 0; i < getHeight(); i++) {
+            for (int j = 0; j < getWidth(); j++) {
+                System.out.println(i + " " + j + " " + collisionLayer.getCell(j, i));
+                if (collisionLayer.getCell(i, j) == null)
+                    collision[i][j] = false;
+                else
+                    collision[i][j] = true;
+            }
+        }
     }
     
     public void render() {
@@ -55,6 +69,10 @@ public class TileMapLoader {
         }
 
         return npcs;
+    }
+    
+    public boolean isCollideable(int height, int width) {
+        return collision[height][width];
     }
     
     public void dispose() {
